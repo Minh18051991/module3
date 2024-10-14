4a9767c87953ab3eeb588e06d3c1d17997f3e3bf
unknown <minhdhh91@gmail.com>
2024-10-14 11:21:14 +0700

    bài tập ss1

--- ss1/quan_ly_sinh_vien.sql
+++ ss1/quan_ly_sinh_vien.sql
@@ -17,9 +17,24 @@ name varchar(100),
 age int, 
 country varchar(100)
 );
-insert into student (id, name ) 
-value (1,"chánh"),(2, "minh");
-alter table student add column(address  varchar(250));
-update student set name ="minh3" where id =2;
-select * from student; 
+alter table student add column(address  varchar(250)) ;
+insert into student (id, name, address ) 
+value (1,"chánh","Gia lai"),(2, "minh","đà nẵng"),(3, "dũng", "Hồ chí minh"),(4,"Lan","huế");
+alter table student add column(class varchar(50));
+insert into class(id, name)
+value (1, "A24"),(2, "A25");
+update student set class = "A24" where id = 1;
+update student set class = "A24" where id = 2;
+update student set class = "A24" where id = 3;
+update student set class = "A25" where id = 4;
+insert into teacher(id, name, age, country)
+value(1, "Hằng", 34, "Việt Nam"),(2,"Tony", 24, "France"),(3, "XiaoZhang", 44, "China");
+select * from teacher;
+select * from class;
+select * from student;
+update student set age = 36 where id = 1;
+update student set age = 34 where id = 2;
+update student set age = 24 where id = 3;
+update student set age = 21 where id = 4;
+select * from student;
 
